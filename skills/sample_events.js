8bb6886b350283c624971fe929531334a88ea362
Glitch (splash-surgeon) <none>
2017-12-03 09:03:40 +0000

    🏋🕴 Checkpoint skills/sample_events.js:577220 glitch_readme.md:577220 readme/MakeHaven.txt:577220 MakeHaven.txt:577220

--- skills/sample_events.js
+++ skills/sample_events.js
@@ -3,7 +3,7 @@ module.exports = function(controller) {
     controller.on('user_channel_join,user_group_join', function(bot, message) {
 
         bot.reply(message, 'Welcome, <@' + message.user + '>' + '! ' +
-                  'For orientation, click the link to install MakeBot: https://slack.com/oauth/authorize?client_id=280783240052.281964108791&scope=bot,channels:write,groups:write,im:write,channels:history,groups:history,im:history,mpim:history');
+                  'For MakeHaven orientation, click the link to install MakeBot: https://slack.com/oauth/authorize?client_id=280783240052.281964108791&scope=bot,channels:write,groups:write,im:write,channels:history,groups:history,im:history,mpim:history');
 
     });
 
