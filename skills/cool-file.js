22cfaa7cfedb99ebd864171aa24cdd87bdf21a2e
Glitch (splash-surgeon) <none>
2017-12-03 02:16:40 +0000

    😻🌘 Checkpoint skills/cool-file.js:576744

--- skills/cool-file.js
+++ skills/cool-file.js
@@ -1,6 +1,6 @@
 /*
 
-Botkit Studio Skill module to enhance the "channel_join" script
+Botkit Studio Skill module to enhance the "onboarding" script
 
 */
 
@@ -9,15 +9,17 @@ module.exports = function(controller) {
     // define a before hook
     // you may define multiple before hooks. they will run in the order they are defined.
     // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudiobefore
-    controller.studio.before('channel_join', function(convo, next) {
+    controller.studio.before('onboarding', function(convo, next) {
 
         // do some preparation before the conversation starts...
         // for example, set variables to be used in the message templates
         // convo.setVar('foo','bar');
 
-        console.log('BEFORE: channel_join');
+        console.log('BEFORE: onboarding');
         // don't forget to call next, or your conversation will never continue.
         next();
+      
+      
 
     });
 
@@ -28,36 +30,37 @@ module.exports = function(controller) {
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
     // Before the default thread starts, run this:
-    controller.studio.beforeThread('channel_join','default', function(convo, next) {
+    controller.studio.beforeThread('onboarding','default', function(convo, next) {
 
         /// do something fun and useful
         // convo.setVar('name','value');
 
-        console.log('In the script *channel_join*, about to start the thread *default*');
+        console.log('In the script *onboarding*, about to start the thread *default*');
 
         // always call next!
         next();
     });
 
     // Before the timeout thread starts, run this:
-    controller.studio.beforeThread('channel_join','timeout', function(convo, next) {
+    controller.studio.beforeThread('onboarding','timeout', function(convo, next) {
 
         /// do something fun and useful
         // convo.setVar('name','value');
 
-        console.log('In the script *channel_join*, about to start the thread *timeout*');
+        console.log('In the script *onboarding*, about to start the thread *timeout*');
 
         // always call next!
         next();
     });
 
-
+   
+  
     // define an after hook
     // you may define multiple after hooks. they will run in the order they are defined.
     // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudioafter
-    controller.studio.after('channel_join', function(convo, next) {
+    controller.studio.after('onboarding', function(convo, next) {
 
-        console.log('AFTER: channel_join');
+        console.log('AFTER: onboarding');
 
         // handle the outcome of the convo
         if (convo.successful()) {
