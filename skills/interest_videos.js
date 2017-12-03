90d5d2776f83b5ac94d7546aaa91ad6abf490f67
Glitch (splash-surgeon) <none>
2017-12-03 01:43:40 +0000

    🐪💷 Checkpoint skills/interest_videos.js:577220

--- skills/interest_videos.js
+++ skills/interest_videos.js
@@ -20,22 +20,23 @@ module.exports = function(controller) {
                   if(response.text == '1'){
                     convo.say('Cool, I like laser cutting too!');
                     convo.say('Here are some awesome laser cutting videos that might be helpful:\n');
-                    convo.say('https://www.youtube.com/watch?v=4TaAvGx8crc \n');
-                    convo.say('https://www.youtube.com/watch?v=jO7bVcIew-w \n');
+                    convo.say('Learn the basics of laser cutting\n https://www.youtube.com/watch?v=4TaAvGx8crc\n');
+                    convo.say('Time lapse of laser cutting project\n https://www.youtube.com/watch?v=jO7bVcIew-w\n');
+                    convo.say('Here you can get your laser cutting badge\n https://www.makehaven.org/badges/laser-cutter\n')
                     convo.next();
                   }
                   else if(response.text == '2'){
-                    convo.say('Cool, I like #D printing too!');
+                    convo.say('Cool, I like 3D printing too!');
                     convo.say('Here is a great 3D printing video that you might enjoy:\n');
-                    convo.say('https://www.youtube.com/watch?v=Sc1SwWCKy4c \n');
+                    convo.say('3D printing overview\n https://www.youtube.com/watch?v=Sc1SwWCKy4c \n');
                     convo.next();
                   }
                   else if(response.text == '3'){
                     convo.say('Cool, I like woodworking too!');
                     convo.say('These woodworking videos are a perfect place to start your next project:\n');
-                    convo.say('https://www.youtube.com/watch?v=iEkYFWndn9g -Get yourself oriented in the wood shop!\n');
-                    convo.say('https://www.youtube.com/watch?v=yVvgFxSidic -Learn the ins and outs of hand planing!\n');
-                    convo.say('https://www.youtube.com/watch?v=LHeVOy5B9ds -Watch the formation of a wooden handheld game console! \n');
+                    convo.say('Get yourself oriented in the wood shop!\n https://www.youtube.com/watch?v=iEkYFWndn9g \n');
+                    convo.say('Learn the ins and outs of hand planing!\n https://www.youtube.com/watch?v=yVvgFxSidic \n');
+                    convo.say('Watch the formation of a wooden handheld game console!\n https://www.youtube.com/watch?v=LHeVOy5B9ds ');
                     convo.next();
                   }
                   else{
